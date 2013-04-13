a2681636d3d013794c014bfcf31a43bf48299fa1
Jay Kid <cyph3rfox@gmail.com>
2013-04-13 17:11:55 +0200

    initial skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoppa::Application.config.secret_token = '4a982b0e61cf2eac5ab7056074bedb24818ece5c8df8db00370c90090ca28c334ae54b2c3d0ca4221d93984c2c2df83c3ab6301216200277abafe2211fa1b232'
